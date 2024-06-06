84586271f57df7be8345c27298c4f33c51a61c1b
Philipp Samfass <philipp.samfass@amd.com>
2024-06-06 05:24:27 -0700

    tmpfix(build system, get_cccl.cmake): Add temporary workaround for CCCL not being available yet for HIP/AMD and in HIP's rapids-cmake. Instead, configure hipCub, libhipcxx and thrust independently.

--- cpp/cmake/thirdparty/get_cccl.cmake
+++ cpp/cmake/thirdparty/get_cccl.cmake
@@ -12,10 +12,46 @@
 # the License.
 # =============================================================================
 
+# Modifications Copyright (c) 2024-2025 Advanced Micro Devices, Inc.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 # Use CPM to find or clone CCCL
 function(find_and_configure_cccl)
-        include(${rapids-cmake-dir}/cpm/cccl.cmake)
-        rapids_cpm_cccl(BUILD_EXPORT_SET raft-exports INSTALL_EXPORT_SET raft-exports)
+  if(CUDA_BACKEND)     
+    include(${rapids-cmake-dir}/cpm/cccl.cmake)
+    rapids_cpm_cccl(BUILD_EXPORT_SET raft-exports INSTALL_EXPORT_SET raft-exports)
+  else()
+    # TODO(HIP/AMD): simply further when CCCL is available on AMD and configurable in
+    # rapids-cmake
+    add_library(CCCL::CCCL INTERFACE IMPORTED GLOBAL)   
+
+    include(${rapids-cmake-dir}/cpm/rocthrust.cmake)
+    rapids_cpm_rocthrust(BUILD_EXPORT_SET raft-exports INSTALL_EXPORT_SET raft-exports)
+
+
+    include(${rapids-cmake-dir}/cpm/libhipcxx.cmake)
+    rapids_cpm_libhipcxx(BUILD_EXPORT_SET raft-exports INSTALL_EXPORT_SET raft-exports)
+    add_library(CCCL::libhipcxx ALIAS _libhipcxx_libhipcxx)
+
+    # TODO(HIP/AMD): it would be good to configure hipcub with rapids-cmake, too.
+    find_package(hipcub REQUIRED CONFIG PATHS "/opt/rocm/hipcub")
+    add_library(CCCL::CUB ALIAS hip::hipcub)
+    # TODO(HIP/AMD): add CUB to raft-exports?
+  endif()
 endfunction()
 
 find_and_configure_cccl()
