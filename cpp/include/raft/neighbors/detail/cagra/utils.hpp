26cd90cbfc155108e5ec7e678c0d871aea379d4d
Philipp Samfaß <psamfass@amd.com>
2024-06-10 15:59:48 +0000

    feat/port (reverse hipification of cuda.h/cuda_runtime.h): Add "reverse" hipification header to map CUDA API calls to HIP API.

--- cpp/include/raft/neighbors/detail/cagra/utils.hpp
+++ cpp/include/raft/neighbors/detail/cagra/utils.hpp
@@ -13,6 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/*
+ * Modifications Copyright (c) 2024 Advanced Micro Devices, Inc.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #pragma once
 
 #include <raft/core/detail/macros.hpp>
@@ -22,8 +41,12 @@
 
 #include <rmm/resource_ref.hpp>
 
+#ifdef __HIP_PLATFORM_AMD__
+#include <raft/cuda_runtime.h>
+#else
 #include <cuda.h>
 #include <cuda_fp16.h>
+#endif
 
 #include <cfloat>
 #include <cstdint>
