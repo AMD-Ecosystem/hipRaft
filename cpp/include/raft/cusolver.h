a45d3f5dae7216ca62dcba45b93d4b17e9122cf3
Philipp Samfass <philipp.samfass@amd.com>
2024-06-13 02:59:39 -0700

    feat/fix(hipification, cusolver.h): add missing reverse hipification definitions

--- cpp/include/raft/cusolver.h
+++ cpp/include/raft/cusolver.h
@@ -131,6 +131,8 @@
 #define cusolverDnXgesvd hipsolverDnXgesvd
 #define cusolverDnXgesvdjSetMaxSweeps hipsolverDnXgesvdjSetMaxSweeps
 #define cusolverDnXgesvdjSetTolerance hipsolverDnXgesvdjSetTolerance
+#define cusolverDnxgesvdr hipsolverDnxgesvdr
+#define cusolverDnxgesvdr_bufferSize hipsolverDnxgesvdr_bufferSize
 #define cusolverDnXsyevjGetSweeps hipsolverDnXsyevjGetSweeps
 #define cusolverDnXsyevjSetMaxSweeps hipsolverDnXsyevjSetMaxSweeps
 #define cusolverDnXsyevjSetTolerance hipsolverDnXsyevjSetTolerance
