4a80531aa3fc1909e2f922368fa6f4b68b26a1e0
sukchoud <Sukriti.Choudhary@amd.com>
2024-11-15 16:39:51 -0800

    feat/port(test files): enable tests for matrix

--- cpp/tests/matrix/columnSort.cu
+++ cpp/tests/matrix/columnSort.cu
@@ -13,6 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Modifications Copyright (c) 2024 Advanced Micro Devices, Inc.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include "../test_utils.cuh"
 
@@ -72,7 +90,7 @@ class ColumnSort : public ::testing::TestWithParam<columnSort<T>> {
 
   void SetUp() override
   {
-    //TODO(HIP/AMD): Disabled due to range check issue. Please see internal issue 8
+    //TODO(HIP/AMD): Unsupported test. Please see internal issue 1x
     GTEST_SKIP() << "Skipping test as currently not supported";
     params  = ::testing::TestWithParam<columnSort<T>>::GetParam();
     int len = params.n_row * params.n_col;
--- cpp/tests/matrix/select_k.cu
+++ cpp/tests/matrix/select_k.cu
@@ -13,6 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Modifications Copyright (c) 2024 Advanced Micro Devices, Inc.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #include "select_k.cuh"
 
 namespace raft::matrix {
@@ -104,7 +122,8 @@ INSTANTIATE_TEST_CASE_P(                          // NOLINT
                                    SelectAlgo::kRadix11bitsExtraPass,
                                    SelectAlgo::kWarpImmediate,
                                    SelectAlgo::kWarpFiltered)));
-                                  // SelectAlgo::kWarpDistributed,
+                                   //TODO(HIP/AMD): Unsupported test. Please see internal issue 1x
+                                   //SelectAlgo::kWarpDistributed,
                                    //SelectAlgo::kWarpDistributedShm)));
 
 using ReferencedRandomDoubleSizeT =
@@ -119,11 +138,13 @@ INSTANTIATE_TEST_CASE_P(                             // NOLINT
                                    SelectAlgo::kRadix11bitsExtraPass,
                                    SelectAlgo::kWarpImmediate,
                                    SelectAlgo::kWarpFiltered)));
+                                   //TODO(HIP/AMD): Unsupported test. Please see internal issue 1x
                                    //SelectAlgo::kWarpDistributed,
                                    //SelectAlgo::kWarpDistributedShm)));
 
 using ReferencedRandomDoubleInt =
   SelectK<double, uint32_t, with_ref<SelectAlgo::kRadix11bits>::params_random>;
+//TODO(HIP/AMD): Unsupported test. Please see internal issue 1x  
 TEST_P(ReferencedRandomDoubleInt, LargeSize) { GTEST_SKIP(); run(); }  // NOLINT
 
 INSTANTIATE_TEST_CASE_P(                                 // NOLINT
