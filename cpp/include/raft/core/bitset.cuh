84f8c84037ea697cc9927c56d29aef26104519d4
gpinkert <gpinkert@amd.com>
2025-02-05 21:18:48 +0000

    feat(bit) All bitset functionality compiles
    The 24.10 upstream introduced new functionality to the bitset type.
    We now have repeat, sparsity, and eval_n_elements and test coverage
    for those APIs.

--- cpp/include/raft/core/bitset.cuh
+++ cpp/include/raft/core/bitset.cuh
@@ -14,6 +14,25 @@
  * limitations under the License.
  */
 
+/*
+ * Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #pragma once
 
 #include <raft/core/bitset.hpp>
@@ -141,7 +160,12 @@ RAFT_KERNEL bitset_repeat_kernel(const bitset_t* src,
         bitset_t next_value = src[(src_word_idx + 1) % src_size];
         src_value |= (next_value << (bits_per_element - src_offset));
       }
-      src_value &= ((bitset_t{1} << remaining_bits) - 1);
+
+      const auto extra_bits_need_masking = remaining_bits < bits_per_element;
+      if (extra_bits_need_masking) {
+        // It is UB to shift a type by its total number of bits
+        src_value &= (bitset_t{1} << remaining_bits) - 1;
+      }
       result |= (src_value << bit_written);
       bit_written += remaining_bits;
     }
