14a6809298e18225ed5543c2aa20db55bcddde29
Kevin Joseph <kevin.joseph@amd.com>
2025-03-12 00:36:12 +0000

    Add missing member functions of vector_view_t
    This was caught by compiling the unit tests with AMD clang version 19.0.0
    which is the version of Clang that ships with ROCm 6.4.
    
    This was previously not caught as the older compiler which shipped with
    ROCm 6.3 did not flag this.

--- cpp/include/raft/spectral/detail/matrix_wrappers.hpp
+++ cpp/include/raft/spectral/detail/matrix_wrappers.hpp
@@ -33,6 +33,25 @@
  * THE SOFTWARE.
  */
 
+/*
+ * Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #pragma once
 
 #include <raft/core/resource/cublas_handle.hpp>
@@ -111,6 +130,14 @@ template <typename value_type>
 struct vector_view_t {
   value_type* buffer_;
   size_type size_;
+
+  value_type* raw() const { return this->raw(); }
+
+  size_type size() const { return this->size(); }
+
+  value_type* raw() { return buffer_; }
+
+  size_type size() { return size_; }
 
   vector_view_t(value_type* buffer, size_type sz) : buffer_(buffer), size_(sz) {}
 
