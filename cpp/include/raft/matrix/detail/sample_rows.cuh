7a93f3a4bbb9926ec03f955e416a1872a2e5235c
Tamas Bela Feher <tfeher@nvidia.com>
2025-05-19 12:07:58 +0200

    Prefer host gather when dataset is available both on host and device (#2671)
    The `raft::matrix::sample_rows` function can select rows both from host and device datasets. This PR changes how the case is handled when the dataset is accessible both from host and device.
    
    When the dataset pointer is accessible both on host and device, then in theory we can subsample it both with our CPU and GPU implementation. In practice,  it is likely that the data is physically located in host memory, therefore it is preferred to process it on the CPU side.
    
    This has significance when the dataset is larger than GPU memory. In that case subsampling using the GPU kernels would lead to page trashing and low performance. In some HMM implementations this can also lead to application crash.
    
    This PR fixes the problem by preferring CPU side subsampling when the dataset is accessible on the CPU.
    
    Authors:
      - Tamas Bela Feher (https://github.com/tfeher)
      - Corey J. Nolet (https://github.com/cjnolet)
    
    Approvers:
      - Artem M. Chirkin (https://github.com/achirkin)
    
    URL: https://github.com/rapidsai/raft/pull/2671

--- cpp/include/raft/matrix/detail/sample_rows.cuh
+++ cpp/include/raft/matrix/detail/sample_rows.cuh
@@ -50,14 +50,57 @@
 
 namespace raft::matrix::detail {
 
+template <typename T, typename IdxT = int64_t>
+void gather_host_path(raft::resources const& res,
+                      const T* input,
+                      IdxT n_dim,
+                      IdxT ld,
+                      IdxT n_rows_input,
+                      raft::device_matrix_view<T, IdxT> output,
+                      raft::device_vector<IdxT, IdxT> const& train_indices)
+{
+#ifdef __HIP_PLATFORM_AMD__
+  // (HIP/AMD) In the code path below when the input dataset is on the host, raft::matrix::gather
+  // utilizes pinned memory as a staging area before gathering rows of the input dataset to the
+  // output. This is done to avoid the overhead of copying the entire input dataset to the device.
+  // However, the pinned memory usage on AMD systems with unified memory architectures like the
+  // MI300A leads to incorrect results. The workaround is to allocate a device buffer and copy the
+  // input dataset to it before calling raft::matrix::gather.
+  // See internal issue 129.
+  int current_device = -1;
+  RAFT_CUDA_TRY(hipGetDevice(&current_device));
+  hipDeviceProp_t device_prop{};
+  RAFT_CUDA_TRY(hipGetDeviceProperties(&device_prop, current_device));
+  if (device_prop.unifiedAddressing) {
+    rmm::device_uvector<T> backing_storage(n_rows_input * ld,
+                                            raft::resource::get_cuda_stream(res),
+                                            raft::resource::get_large_workspace_resource(res));
+    raft::copy(
+      backing_storage.data(), input, n_rows_input * ld, raft::resource::get_cuda_stream(res));
+    auto device_dataset =
+      raft::make_device_strided_matrix_view<const T, IdxT>(backing_storage.data(), n_rows_input, n_dim, ld);
+    raft::matrix::gather(
+      res, device_dataset, raft::make_const_mdspan(train_indices.view()), output);
+  } else {
+    // (HIP/AMD) Not using unified addressing, should be safe to use the host path
+    auto dataset =
+      raft::make_host_strided_matrix_view<const T, IdxT>(input, n_rows_input, n_dim, ld);
+    raft::matrix::detail::gather(res, dataset, make_const_mdspan(train_indices.view()), output);
+  }
+#else
+  auto dataset = raft::make_host_strided_matrix_view<const T, IdxT>(input, n_rows_input, n_dim, ld);
+  raft::matrix::detail::gather(res, dataset, make_const_mdspan(train_indices.view()), output);
+#endif
+}
+
 /** Select rows randomly from input and copy to output. */
 template <typename T, typename IdxT = int64_t>
 void sample_rows(raft::resources const& res,
-                 random::RngState random_state,
-                 const T* input,
-                 IdxT ld,
-                 IdxT n_rows_input,
-                 raft::device_matrix_view<T, IdxT> output)
+                  random::RngState random_state,
+                  const T* input,
+                  IdxT ld,
+                  IdxT n_rows_input,
+                  raft::device_matrix_view<T, IdxT> output)
 {
   IdxT n_dim     = output.extent(1);
   IdxT n_samples = output.extent(0);
@@ -67,47 +110,20 @@ void sample_rows(raft::resources const& res,
 
   cudaPointerAttributes attr;
   RAFT_CUDA_TRY(cudaPointerGetAttributes(&attr, input));
-  T* ptr = reinterpret_cast<T*>(attr.devicePointer);
-  if (ptr != nullptr) {
-    raft::matrix::gather(
-      res,
-      raft::make_device_strided_matrix_view<const T, IdxT>(ptr, n_rows_input, n_dim, ld),
-      raft::make_const_mdspan(train_indices.view()),
-      output);
-  } else {
-#ifdef __HIP_PLATFORM_AMD__
-    // (HIP/AMD) In the code path below when the input dataset is on the host, raft::matrix::gather
-    // utilizes pinned memory as a staging area before gathering rows of the input dataset to the
-    // output. This is done to avoid the overhead of copying the entire input dataset to the device.
-    // However, the pinned memory usage on AMD systems with unified memory architectures like the
-    // MI300A leads to incorrect results. The workaround is to allocate a device buffer and copy the
-    // input dataset to it before calling raft::matrix::gather.
-    // See internal issue 129.
-    int current_device = -1;
-    RAFT_CUDA_TRY(hipGetDevice(&current_device));
-    hipDeviceProp_t device_prop{};
-    RAFT_CUDA_TRY(hipGetDeviceProperties(&device_prop, current_device));
-    if (device_prop.unifiedAddressing) {
-      rmm::device_uvector<T> backing_storage(n_rows_input * ld,
-                                             raft::resource::get_cuda_stream(res),
-                                             raft::resource::get_large_workspace_resource(res));
-      raft::copy(
-        backing_storage.data(), input, n_rows_input * ld, raft::resource::get_cuda_stream(res));
-      auto device_dataset =
-        raft::make_device_strided_matrix_view<const T, IdxT>(backing_storage.data(), n_rows_input, n_dim, ld);
-      raft::matrix::gather(
-        res, device_dataset, raft::make_const_mdspan(train_indices.view()), output);
-    } else {
-      // (HIP/AMD) Not using unified addressing, should be safe to use the host path
-      auto dataset =
-        raft::make_host_strided_matrix_view<const T, IdxT>(input, n_rows_input, n_dim, ld);
-      raft::matrix::detail::gather(res, dataset, make_const_mdspan(train_indices.view()), output);
-    }
-#else
+  // We can have both valid host and device pointers (systems with HMM or ATS).
+  // In that case the dataset can be larger than GPU memory and gathering on host is preferred.
+  if (attr.hostPointer != nullptr) {
+    T* ptr       = reinterpret_cast<T*>(attr.hostPointer);
+    gather_host_path(res, ptr, n_dim, ld, n_rows_input, output, train_indices);
+  } else if (attr.devicePointer != nullptr) {
+    T* ptr = reinterpret_cast<T*>(attr.devicePointer);
     auto dataset =
-      raft::make_host_strided_matrix_view<const T, IdxT>(input, n_rows_input, n_dim, ld);
-    raft::matrix::detail::gather(res, dataset, make_const_mdspan(train_indices.view()), output);
-#endif
+      raft::make_device_strided_matrix_view<const T, IdxT>(ptr, n_rows_input, n_dim, ld);
+    raft::matrix::gather(res, dataset, raft::make_const_mdspan(train_indices.view()), output);
+  } else {
+    // For older driver versions it can happen that both device and host pointers in `attr` are
+    // invalid. We use the original host pointer in this case.
+    gather_host_path(res, input, n_dim, ld, n_rows_input, output, train_indices);
   }
 }
 
