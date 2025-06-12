b088c459d49d359c441441102584a0661af40179
Kevin Joseph <kevin.joseph@amd.com>
2025-06-12 18:11:47 +0000

    Revert "WAR: Use old CUDA bindings"
    This reverts commit 806f1394be4642cff545b398fda392371f091aa6.

--- python/pylibraft/pylibraft/common/interruptible.pyx
+++ python/pylibraft/pylibraft/common/interruptible.pyx
@@ -14,24 +14,6 @@
 # limitations under the License.
 #
 
-# Modifications Copyright (c) 2025 Advanced Micro Devices, Inc.
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
-
 # cython: profile=False
 # distutils: language = c++
 # cython: embedsignature = True
@@ -40,11 +22,7 @@
 import contextlib
 import signal
 
-# TODO(HIP-PYTHON) Once https://github.com/AMD-AI/hip-python/pull/3 has merged
-# and made public, remove the line below
-
-from cuda.ccuda cimport cudaStream_t
-# from cuda.bindings.cyruntime cimport cudaStream_t
+from cuda.bindings.cyruntime cimport cudaStream_t
 from cython.operator cimport dereference
 
 from rmm.librmm.cuda_stream_view cimport cuda_stream_view
