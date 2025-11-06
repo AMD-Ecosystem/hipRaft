c8ae8153042c8bf8e10465edc29a30f224dc95c3
Kevin Joseph <kevin.joseph@amd.com>
2025-11-06 18:10:00 +0000

    [AMD][fix]Pre-commit fixes

--- cpp/include/raft/matrix/detail/sample_rows.cuh
+++ cpp/include/raft/matrix/detail/sample_rows.cuh
@@ -73,12 +73,12 @@ void gather_host_path(raft::resources const& res,
   RAFT_CUDA_TRY(hipGetDeviceProperties(&device_prop, current_device));
   if (device_prop.unifiedAddressing) {
     rmm::device_uvector<T> backing_storage(n_rows_input * ld,
-                                            raft::resource::get_cuda_stream(res),
-                                            raft::resource::get_large_workspace_resource(res));
+                                           raft::resource::get_cuda_stream(res),
+                                           raft::resource::get_large_workspace_resource(res));
     raft::copy(
       backing_storage.data(), input, n_rows_input * ld, raft::resource::get_cuda_stream(res));
-    auto device_dataset =
-      raft::make_device_strided_matrix_view<const T, IdxT>(backing_storage.data(), n_rows_input, n_dim, ld);
+    auto device_dataset = raft::make_device_strided_matrix_view<const T, IdxT>(
+      backing_storage.data(), n_rows_input, n_dim, ld);
     raft::matrix::gather(
       res, device_dataset, raft::make_const_mdspan(train_indices.view()), output);
   } else {
@@ -96,11 +96,11 @@ void gather_host_path(raft::resources const& res,
 /** Select rows randomly from input and copy to output. */
 template <typename T, typename IdxT = int64_t>
 void sample_rows(raft::resources const& res,
-                  random::RngState random_state,
-                  const T* input,
-                  IdxT ld,
-                  IdxT n_rows_input,
-                  raft::device_matrix_view<T, IdxT> output)
+                 random::RngState random_state,
+                 const T* input,
+                 IdxT ld,
+                 IdxT n_rows_input,
+                 raft::device_matrix_view<T, IdxT> output)
 {
   IdxT n_dim     = output.extent(1);
   IdxT n_samples = output.extent(0);
@@ -113,7 +113,7 @@ void sample_rows(raft::resources const& res,
   // We can have both valid host and device pointers (systems with HMM or ATS).
   // In that case the dataset can be larger than GPU memory and gathering on host is preferred.
   if (attr.hostPointer != nullptr) {
-    T* ptr       = reinterpret_cast<T*>(attr.hostPointer);
+    T* ptr = reinterpret_cast<T*>(attr.hostPointer);
     gather_host_path(res, ptr, n_dim, ld, n_rows_input, output, train_indices);
   } else if (attr.devicePointer != nullptr) {
     T* ptr = reinterpret_cast<T*>(attr.devicePointer);
--- cpp/tests/linalg/transpose.cu
+++ cpp/tests/linalg/transpose.cu
@@ -269,9 +269,9 @@ namespace transpose_extra_test {
 template <typename T, typename IndexType, typename LayoutPolicy>
 [[nodiscard]] auto transpose(raft::resources const& handle,
                              device_matrix_view<T, IndexType, LayoutPolicy> in)
-  -> std::enable_if_t<(std::is_floating_point_v<T> || std::is_same_v<T, half>)&&(
-                        std::is_same_v<LayoutPolicy, layout_c_contiguous> ||
-                        std::is_same_v<LayoutPolicy, layout_f_contiguous>),
+  -> std::enable_if_t<(std::is_floating_point_v<T> || std::is_same_v<T, half>) &&
+                        (std::is_same_v<LayoutPolicy, layout_c_contiguous> ||
+                         std::is_same_v<LayoutPolicy, layout_f_contiguous>),
                       device_matrix<T, IndexType, LayoutPolicy>>
 {
   auto out = make_device_matrix<T, IndexType, LayoutPolicy>(handle, in.extent(1), in.extent(0));
