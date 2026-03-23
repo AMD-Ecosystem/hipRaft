1f812d8f025774ab77785cde6c28e52abdac63b3
randyh62 <randy.hartgrove@amd.com>
2026-03-23 13:44:33 -0700

    Update release version to 26.03

--- docs_amd/conf.py
+++ docs_amd/conf.py
@@ -32,7 +32,7 @@ left_nav_title = f"hipRAFT {version_number} documentation"
 # for PDF output on Read the Docs
 project = "hipRAFT"
 author = "Advanced Micro Devices, Inc."
-copyright = "Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved."
+copyright = "Copyright (c) 2026 Advanced Micro Devices, Inc. All rights reserved."
 version = version_number
 release = version_number
 cpp_maximum_signature_line_length = 10
@@ -43,7 +43,7 @@ all_article_info_author = ""
 external_projects_current_project = "hipRAFT"
 
 html_context = {
-    "docs_header_version": "25.10"
+    "docs_header_version": "26.03"
 }
 html_theme = "rocm_docs_theme"
 html_theme_options = {
